a5cbf6cb08a20a8edabd80af0067cb1da22a5224
Louis Chatriot <louis.chatriot@gmail.com>
2015-03-24 16:42:51 +0100

    Able to run tests as I want

--- test/rideData.test.js
+++ test/rideData.test.js
@@ -8,8 +8,16 @@ var should = require('chai').should()
 
 describe('Ride data', function () {
 
-  beforeEach(function () {
+  beforeEach(function (done) {
     config.database.should.equal('./data/test-workspace/');   // Having to do that is freaking stupid but npm leaves me no choice.
+    rideData.removeAll(function (err) {
+      assert.iNull(err);
+      db.find({}, function (err, docs) {
+        assert.isNull(err);
+        docs.length.should.equal(0);
+        return done();
+      });
+    });
   });
 
   describe('Inserting one ride', function () {
